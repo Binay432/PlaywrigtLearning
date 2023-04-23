a8aa2ccf368bf1d88f74eb4362a7883465240a9a
Binay32 <sahbinay432@gmail.com>
2023-04-23 21:49:57 +0545

    configure ss and video settings and tesr script

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-
+  testMatch:"tests/loginTest.spec.ts",
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -30,6 +30,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot:"only-on-failure",
+    video:"retain-on-failure",
   },
 
   /* Configure projects for major browsers */
